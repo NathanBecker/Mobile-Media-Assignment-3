b89f6f128e2637060ad36b756d2b8d1b9deae548
NathanBecker <nathanbeckerdesign@gmail.com>
2012-04-16 20:31:41 -0400

    Assignment 5 to the first commit point (the first half of the pdf is completed and working)

---
+++ Restaurants/ReviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReviewViewController.h
+//  Restaurants
+//
+//  Created by Nathan Becker on 4/16/12.
+//  Copyright (c) 2012 Parsons New School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReviewViewController : UITableViewController
+
+@end
